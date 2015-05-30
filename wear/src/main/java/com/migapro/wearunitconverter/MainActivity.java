7b09fc981da117df612b58572eff47aa513f6bc7
miga <miguel.g.kano@gmail.com>
2015-05-30 13:26:13 -0500

    Listen to touches

--- wear/src/main/java/com/migapro/wearunitconverter/MainActivity.java
+++ wear/src/main/java/com/migapro/wearunitconverter/MainActivity.java
@@ -2,6 +2,9 @@ package com.migapro.wearunitconverter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView unitFrom = (TextView) findViewById(R.id.unit_from);
+        unitFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "unit from", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView numFrom = (TextView) findViewById(R.id.num_from);
+        numFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "number from", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextView unitTo = (TextView) findViewById(R.id.unit_to);
+        unitTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "unit to", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
