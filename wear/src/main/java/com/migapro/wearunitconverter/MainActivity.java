a0451767897972c0243812301317387190a0d2e0
miga <miguel.g.kano@gmail.com>
2015-05-30 15:04:45 -0500

    Integrate ButterKnife

--- wear/src/main/java/com/migapro/wearunitconverter/MainActivity.java
+++ wear/src/main/java/com/migapro/wearunitconverter/MainActivity.java
@@ -3,42 +3,42 @@ package com.migapro.wearunitconverter;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
 public class MainActivity extends Activity {
+
+    @InjectView(R.id.unit_from) TextView unitFromLabel;
+    @InjectView(R.id.num_from) TextView numFromLabel;
+    @InjectView(R.id.unit_to) TextView unitToLabel;
+    @InjectView(R.id.num_to) TextView numToLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView unitFrom = (TextView) findViewById(R.id.unit_from);
-        unitFrom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "unit from", Toast.LENGTH_SHORT).show();
-            }
-        });
+        ButterKnife.inject(this);
+    }
 
-        TextView numFrom = (TextView) findViewById(R.id.num_from);
-        numFrom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "number from", Toast.LENGTH_SHORT).show();
-                FragmentManager fm = getFragmentManager();
-                NumberInputDialogFragment numberInputDialog = new NumberInputDialogFragment();
-                numberInputDialog.show(fm, "fragment_number_input");
-            }
-        });
+    @OnClick(R.id.unit_from)
+    public void onUnitFromClick() {
+        Toast.makeText(getApplicationContext(), "unit from", Toast.LENGTH_SHORT).show();
+    }
 
-        TextView unitTo = (TextView) findViewById(R.id.unit_to);
-        unitTo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "unit to", Toast.LENGTH_SHORT).show();
-            }
-        });
+    @OnClick(R.id.num_from)
+    public void onNumFromClick() {
+        FragmentManager fm = getFragmentManager();
+        NumberInputDialogFragment numberInputDialog = new NumberInputDialogFragment();
+        numberInputDialog.show(fm, "fragment_number_input");
+    }
+
+    @OnClick(R.id.unit_to)
+    public void onUnitToClick() {
+        Toast.makeText(getApplicationContext(), "unit to", Toast.LENGTH_SHORT).show();
     }
 }
