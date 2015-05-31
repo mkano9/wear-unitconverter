059ca74a439082abc78baf50a9266b5cbbeff7c7
miga <miguel.g.kano@gmail.com>
2015-05-30 20:45:42 -0500

    Handle scenario entering number on '-0' input

--- wear/src/main/java/com/migapro/wearunitconverter/model/NumberKey.java
+++ wear/src/main/java/com/migapro/wearunitconverter/model/NumberKey.java
@@ -12,7 +12,16 @@ public class NumberKey extends NumberPadKey {
     public String processKey(String currentInput) {
         if (currentInput.length() == 1 && currentInput.charAt(0) == '0') {
             return mNumber;
+        } else if (isNegativeZero(currentInput)) {
+            return '-' + mNumber;
         }
+
         return currentInput + mNumber;
+    }
+
+    private boolean isNegativeZero(String number) {
+        return (number.length() == 2
+                && number.charAt(0) == '-'
+                && number.charAt(1) == '0');
     }
 }
