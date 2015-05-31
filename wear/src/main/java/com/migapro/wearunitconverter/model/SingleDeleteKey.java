58ac1909537043d6979ea9ff9688854ed075d090
miga <miguel.g.kano@gmail.com>
2015-05-30 20:31:38 -0500

    Delete '-' if it is the only char left

--- wear/src/main/java/com/migapro/wearunitconverter/model/SingleDeleteKey.java
+++ wear/src/main/java/com/migapro/wearunitconverter/model/SingleDeleteKey.java
@@ -6,7 +6,8 @@ public class SingleDeleteKey extends NumberPadKey {
         if (!currentInput.isEmpty()) {
             String processedInput = currentInput.substring(0, currentInput.length() - 1);
 
-            if (processedInput.isEmpty()) {
+            if (processedInput.isEmpty() ||
+                    (processedInput.length() == 1 && processedInput.charAt(0) == '-')) {
                 processedInput = "0";
             }
             return processedInput;
